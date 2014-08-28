0c2557929e6ac7a6107f89b5d34b944347d75330
Catherine Tran & Erica Gordon <student@epicodus.com>
2014-08-28 13:47:39 -0700

    add question file and question spec file.  Make specs pass to create associations between questions and a survey.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,13 @@ require 'pg'
 require 'shoulda-matchers'
 
 require 'survey'
+require 'question'
 
 ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))['test'])
 
 RSpec.configure do | config |
   config.after(:each) do
     Survey.all.each{|survey| survey.destroy}
+    Question.all.each{ |question| question.destroy }
   end
 end
