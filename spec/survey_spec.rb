e9d7f81084d5b52f475246621544eccead4edf0a
Catherine Tran & Erica Gordon <student@epicodus.com>
2014-08-28 14:19:10 -0700

    Add validation of a name to survey model. Make spec pass.

--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Survey do
   it { should have_many :questions}
+
+  it 'validates the presence of name' do
+    survey = Survey.new({:name => ''})
+    expect(survey.save).to eq false
+  end
 end
+
+
