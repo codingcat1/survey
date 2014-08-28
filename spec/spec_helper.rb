5bd1ef6786174ebee076f1cceb630757781cc2a3
Catherine Tran & Erica Gordon <student@epicodus.com>
2014-08-28 12:01:14 -0700

    create requires and rspec configuration loop within spec_helper file.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'active_record'
+require 'rspec'
+require 'pg'
+require 'shoulda-matchers'
+
+require 'survey'
+
+ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))['test'])
+
+RSpec.configure do | config |
+  config.after(:each) do
+    Survey.all.each{|survey| survey.destroy}
+  end
+end
